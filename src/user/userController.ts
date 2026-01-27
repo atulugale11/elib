b1687d2885021d4378d26868d99d63886fb7b7f4
Atul Ugale (BLR GSS) <atulgu@nousinfo.com>
2026-01-27 22:50:36 +0530

    added error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import bcrypt = require("bcrypt");
 import userModal from "./userModal";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (
    req: express.Request,
@@ -11,32 +12,45 @@ const createUser = async (
    next: express.NextFunction,
 ) => {
    const { name, email, password } = req.body;
-   const user = await userModal.findOne({ email });
 
+   try {
+      const user = await userModal.findOne({ email });
+      if (user) {
+         const error = createHttpError(409, "User already exists"); // Conflict
+         return next(error); // passing the error to the global error handler middleware
+      }
+   } catch (err) {
+      next(createHttpError(500, "Error while getting user"));
+   }
    const hashedPassword = await bcrypt.hash(password, 10);
 
-   if (user) {
-      const error = createHttpError(409, "User already exists"); // Conflict
-      return next(error); // passing the error to the global error handler middleware
-   }
    //validation
    if (!name || !email || !password) {
       const error = createHttpError(400, "All fields are required"); // Bad Request
       return next(error); // passing the error to the global error handler middleware
    }
 
-   const newUser = await userModal.create({
-      name,
-      email,
-      password: hashedPassword,
-   });
+   let newUser: User;
+   try {
+      newUser = await userModal.create({
+         name,
+         email,
+         password: hashedPassword,
+      });
+   } catch (err) {
+      return next(createHttpError(500, "Error while creating user"));
+   }
 
-   const token = sign({ sub: newUser._id }, config.jswtSecret as string, {
-      expiresIn: "7d",
-      algorithm: "HS256", // HMAC using SHA-256 hash algorithm default is HS256
-   });
+   try {
+      const token = sign({ sub: newUser._id }, config.jswtSecret as string, {
+         expiresIn: "7d",
+         algorithm: "HS256", // HMAC using SHA-256 hash algorithm default is HS256
+      });
+      res.json({ accessToken: token });
+   } catch (err) {
+      return next(createHttpError(500, "Error while creating token"));
+   }
    //logic for creating user
-   res.json({ accessToken: token });
 };
 
 export { createUser };
