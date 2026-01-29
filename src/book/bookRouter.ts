bed50f900c9b9a33756b801042f1c943abb92fa3
Atul Ugale (BLR GSS) <atulgu@nousinfo.com>
2026-01-29 09:07:37 +0530

    setup multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,20 @@
 import express = require("express");
-
+import { createBook } from "./bookController";
+import multer = require("multer");
+import path = require("path");
 const bookRouter = express.Router();
 
-// bookRouter.post("/", createBook);
+const upload = multer({
+   dest: path.resolve(__dirname, "../../public/data/uploads"),
+   limits: { fileSize: 30 * 1024 * 1024 }, // 30 MB file size limit
+});
+bookRouter.post(
+   "/",
+   upload.fields([
+      { name: "coverImage", maxCount: 1 },
+      { name: "file", maxCount: 1 },
+   ]),
+   createBook,
+);
+
 export default bookRouter;
